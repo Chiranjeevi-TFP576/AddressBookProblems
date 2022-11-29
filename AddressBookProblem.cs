13cebeadf62e2ffdcd9c1358b4d933b5463294ad
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2022-11-29 12:27:47 +0530

    AddressBook UC1 to UC6

---
+++ AddressBookProblem.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+
+    public class AddressBookProblem
+    {
+        public List<Contact> personDetails = new List<Contact>();
+
+        //Creating a method for adding contacts in adrressbook
+        public void AddPerson()
+        {
+            Contact person = new Contact();
+            Console.WriteLine("Enter first name");
+            person.firstName = Console.ReadLine();
+            Console.WriteLine("Enter last name");
+            person.lastName = Console.ReadLine();
+            Console.WriteLine("Enter address name");
+            person.address = Console.ReadLine();
+            Console.WriteLine("Enter city name");
+            person.city = Console.ReadLine();
+            Console.WriteLine("Enter state name");
+            person.state = Console.ReadLine();
+            Console.WriteLine("Enter zip name");
+            person.zip = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            person.phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email id");
+            person.email = Console.ReadLine();
+            personDetails.Add(person);
+        }
+
+        //Adding another contact
+        public void MultiPersons()
+        {
+            AddPerson();
+            Console.Write("\n Do You Want To Add Another Contact (Y/N) :  ");
+            string choice = Console.ReadLine();
+            while (choice == "Y" || choice == "y")
+            {
+                AddPerson();
+                Console.Write("\nDo You Want To Add Another Contact (Y/N) :  ");
+                choice = Console.ReadLine();
+            }
+        }
+
+        //Printing the address book details 
+        public void Display()
+        {
+            foreach (Contact person in personDetails)
+            {
+                Console.WriteLine("---Address book details----");
+                Console.WriteLine("First Name:" + person.firstName);
+                Console.WriteLine("Last Name:" + person.lastName);
+                Console.WriteLine("Address:" + person.address);
+                Console.WriteLine("City:" + person.city);
+                Console.WriteLine("State:" + person.state);
+                Console.WriteLine("Zip:" + person.zip);
+                Console.WriteLine("Phone Number:" + person.phoneNumber);
+                Console.WriteLine("Email:" + person.email);
+            }
+        }
+        //Uc3..creating method for editting existing contact in address book
+        public void Edit()
+        {
+            if (personDetails.Count != 0)
+            {
+                Console.WriteLine("Enter contact to modify:");
+                string edit = Console.ReadLine();
+                foreach (var person in personDetails)
+                {
+                    //ToUpper used to convert to uppercase 
+                    if (person.firstName.ToUpper() == edit.ToUpper())
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Enter the option to modify the property: ");
+                            Console.WriteLine("Enter 1 to Change First name ");
+                            Console.WriteLine("Enter 2 to Change Last name ");
+                            Console.WriteLine("Enter 3 to Change Phone Number ");
+                            Console.WriteLine("Enter 4 to Change Address ");
+                            Console.WriteLine("Enter 5 to Change City ");
+                            Console.WriteLine("Enter 6 to Change State ");
+                            Console.WriteLine("Enter 7 to Change Pincode ");
+                            Console.WriteLine("Enter 8 to Exit ");
+
+                            int Option = Convert.ToInt32(Console.ReadLine());
+
+                            //Switch case statement taken to choose desired operation
+                            switch (Option)
+                            {
+                                case 1:
+                                    Console.WriteLine("Enter the New First Name: ");
+                                    person.firstName = Console.ReadLine();
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Enter the New Last Name: ");
+                                    person.lastName = Console.ReadLine();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter the New Phone Number: ");
+                                    person.phoneNumber = Console.ReadLine();
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter the New Address: ");
+                                    person.address = Console.ReadLine();
+                                    break;
+                                case 5:
+                                    Console.WriteLine("Enter the New City: ");
+                                    person.city = Console.ReadLine();
+                                    break;
+                                case 6:
+                                    Console.WriteLine("Enter the New State: ");
+                                    person.state = Console.ReadLine();
+                                    break;
+                                case 7:
+                                    Console.WriteLine("Enter the New Pin Code: ");
+                                    person.zip = Console.ReadLine();
+                                    break;
+                                case 8:
+                                    return;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact does not exist");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Your address book is empty");
+            }
+        }
+
+        //method for deleating or remove a existing contact
+        public void Delete()
+        {
+            Console.WriteLine("Enter the first name to remove persone :");
+            string delete = Console.ReadLine();
+
+            foreach (Contact person in personDetails)
+            {
+                if (person.firstName.ToLower() == delete.ToLower())
+                {
+                    Console.WriteLine("Are you sure you want to delete this contact?(Y/N)");
+                    if (Console.ReadKey().Key == ConsoleKey.Y)
+                    {
+                        personDetails.Remove(person);
+                        Console.WriteLine("\nContact is deleted");
+                    }
+                    Console.WriteLine("\n\nContact Deleted from the List !");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Contact not Found ! ");
+
+                }
+            }
+        }
+        //Any will check for duplicate same firstnamename in database
+        public static bool CheckDuplicate(List<Contact> contacts, string firstName)
+        {
+
+            
+            if (contacts.Count > 0)
+            {
+
+                if (contacts.Any(x => x.firstName.Equals(firstName)))
+                {
+                    Console.WriteLine("Already exist in database");
+                    return true;
+                }
+
+            }
+            return false;
+
+        }
+
+       
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddressBook;
+
+Console.WriteLine("Welcome To Address Book Program");
+
+
+//creating object person1 of AddressBook Class()
+AddressBookProblem person1 = new AddressBookProblem();
+while (true)
+{
+    Console.WriteLine("Choose one operation to perform:\n 1-Add a Contact \n 2-View the AddressBook \n 3-Edit the Address Book \n 4-Delete the person \n 5-Multi Person to AddressBook \n 6-Exit from the Addressbook\n 7CheckDuplicate");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            person1.AddPerson();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 2:
+            person1.Display();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 3:
+            person1.Edit();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 4:
+            person1.Delete();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 5:
+            person1.MultiPersons();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 6:
+            person1.CheckDuplicate();
+            Console.WriteLine("Enter any key to exit");
+            break;
+        case 7:
+            return;
+        default:
+            Console.WriteLine("Please enter a valid input");
+            break;
+    }
+    Console.ReadLine();
+}
+        
+        
